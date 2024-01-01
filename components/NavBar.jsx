7f314625c126635e5bfef4ffa1170b4c8b259dea
amalnathvp <amalnathvp31@gmail.com>
2024-01-01 09:07:47 +0530

    small change

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -86,9 +86,9 @@ export default function NavBar() {
 
     return (
         <>
-            <div className={`w-full ${scrolled ? "bg-indigo-600/70" : "bg-indigo-600"} px-6 h-20 bg-indigo-600 text-white flex items-center justify-between fixed top-0 left-0 z-50`}>
+            <div className={`w-full ${scrolled ? "bg-white-600/70" : "bg-white-600"} px-6 h-20 bg-white text-black flex items-center justify-between fixed top-0 left-0 z-50 shadow-lg`}>
                 <div className='px-2 h-full flex items-center justify-center'>
-                    <p className='uppercase font-semibold tracking-widest text-lg'>JOB-PORTAL</p>
+                    <p className='uppercase font-bold tracking-widest text-lg text-indigo-600'>Hire-All</p>
                 </div>
                 <div className='px-2 h-full hidden items-center justify-center lg:flex'>
                     <Link href={'/'} className="px-3 mx-4 text-base font-medium transition-all duration-700 hover:translate-y-2 uppercase" >Home</Link>
