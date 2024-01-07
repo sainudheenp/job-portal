0f0908623fa560de9d74ac22edd3b6103217e82a
amalnathvp <amalnathvp31@gmail.com>
2024-01-07 12:07:52 +0530

    dlt job pending

--- pages/frontend/applicationDetail/[id].jsx
+++ pages/frontend/applicationDetail/[id].jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { useRouter } from 'next/router';
 import useSWR from 'swr'
 import { get_application_details } from '@/Services/job';
-import { InfinitySpin } from 'react-loader-spinner';
 import NavBar from '@/components/NavBar';
 import { toast } from 'react-toastify';
 import Cookies from 'js-cookie';
